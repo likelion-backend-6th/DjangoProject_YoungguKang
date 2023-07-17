3a13d37fc0b6d3adfb5870aa9696493cca9727a6
Younggu Kang <khkkor3@naver.com>
2023-07-17 15:42:10 +0900

    11

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -10,7 +10,7 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
