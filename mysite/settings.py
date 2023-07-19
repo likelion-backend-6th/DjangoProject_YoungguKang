bae2051f5b209ef8578b13dc0b0334c306ac0d50
Younggu Kang <khkkor3@naver.com>
2023-07-19 14:54:46 +0900

    이메일 콘솔 출력 설정

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,6 +123,8 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 # 이메일 서버 구성
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = ''
