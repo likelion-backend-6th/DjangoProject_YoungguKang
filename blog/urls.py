a155cf96feda5b71196e558cd842dc1574c0624b
Younggu Kang <khkkor3@naver.com>
2023-07-19 15:36:22 +0900

    share.html 템플릿 추가, url 경로 추가

--- blog/urls.py
+++ blog/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
     re_path(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/(?P<day>[0-9]{1,2})/(?P<post>[-\w]+)/$', views.post_detail,
             name='post_detail'),
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
 ]
