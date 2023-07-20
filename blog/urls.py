92594ee4b532e90a89842723288d325ca6775154
Younggu Kang <khkkor3@naver.com>
2023-07-20 10:41:12 +0900

    Commentform 생성, post_comment뷰 함수 추가, url 경로 추가

--- blog/urls.py
+++ blog/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     re_path(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/(?P<day>[0-9]{1,2})/(?P<post>[-\w]+)/$', views.post_detail,
             name='post_detail'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
+    path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
 ]
