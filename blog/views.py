d8f090d49ad865c2a40615a7c3134c1f01f9ee4e
Younggu Kang <khkkor3@naver.com>
2023-07-21 15:15:40 +0900

    Rank, QUery 실습

--- blog/views.py
+++ blog/views.py
@@ -11,6 +11,7 @@ from blog.models import Post
 
 from django.contrib.postgres.search import SearchVector
 from .forms import EmailPostForm, CommentForm, SearchForm
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 
 class PostListView(ListView):
@@ -94,15 +95,18 @@ def post_comment(request, post_id):
 
 
 def post_search(request):
-    form = SearchForm
+    form = SearchForm()
     query = None
     results = []
     if 'query' in request.GET:
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchQuery('title', 'body')
+            search_query = SearchQuery(query)
             results = Post.published.annotate(
-                search=SearchVector('title', 'body'),
+                search=SearchVector,
+                rank=SearchRank(search_vector, search_query)
             ).filter(search=query)
     return render(request,
                   'blog/post/search.html',
